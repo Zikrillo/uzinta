706dd1b929a081f45f228f78e0230856b50f2e08
Zikrillo Abbosov <zikrillo5018@gmail.com>
2022-02-09 20:50:58 +0500

    Добавил навигационную панель и лого

---
+++ sidebar.js
@@ -0,0 +1,5 @@
+let menu  = document.querySelector(".burger__menu");
+let bars  = document.querySelector(".bars");
+bars.addEventListener("click", ()=>{
+    menu.classList.toggle("burger_active");
+})
